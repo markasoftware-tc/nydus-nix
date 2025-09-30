5df1677bb052b30f3d5cdbcab9423d389237173d
Mark Polyakov <mark@Mac.attlocal.net>
2025-09-30 13:43:09 -0400

    path for snapshotter

--- nydus-snapshotter.nix
+++ nydus-snapshotter.nix
@@ -1,8 +1,9 @@
-{ fetchFromGitHub, buildGoModule }:
+{ makeBinaryWrapper, fetchFromGitHub, buildGoModule }:
 
 buildGoModule rec {
   pname = "nydus-snapshotter";
   version = "v0.15.4";
+  nativeBuildInputs = [ makeBinaryWrapper ];
   # weird docker crap
   doCheck = false;
   src = fetchFromGitHub {
@@ -12,4 +13,7 @@ buildGoModule rec {
     hash = "sha256-8Rz9ppaClmmZtMMkDZuQ1YZrXZEwl6ZBHJ5Ju0fpIHc=";
   };
   vendorHash = "sha256-SoZ7yul64Y1a9FFOnrW7V91IGdd021LzFudiFwhVxwA=";
+  postInstall = ''
+    makeWrapper $out/bin/containerd-nydus-grpc $out/bin/containerd-nydus-grpc --prefix PATH : ${lib.makeBinPath [ nydus ] }
+  '';
 }
