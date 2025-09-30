a998ad468268e7083820e29dd51f8f9defcb5edb
Mark Polyakov <mark@Mac.attlocal.net>
2025-09-30 12:59:24 -0400

    hopefully fix it

--- nydusify.nix
+++ nydusify.nix
@@ -1,21 +1,19 @@
 { lib, fetchFromGitHub, buildGoModule, runCommand, makeBinaryWrapper, nydus }:
 
 # to update: Bump the version, then wait for the first hash failure, replace that in fetchFromGitHub, then run again, and put the hash in cargoHash
-let nydusifyRawPackage = buildGoModule rec {
-      pname = "nydusify";
-      version = "2.3.7";
-      nativeBuildInputs = [ makeBinaryWrapper ];
-      src = fetchFromGitHub {
-        owner = "dragonflyoss";
-        repo = "nydus";
-        rev = "v${version}";
-        hash = "sha256-dVPfxsM/mduCopc2t+60+MDO98DRYvpNB+9/gUoXTSc=";
-      } + "/contrib/nydusify";
-      vendorHash = "sha256-SmDOmCMinTg72P8n4c1HnwKpt/CVeIlqdFcp5LHm2tA=";
-      postFixup = "makeWrapper $out/bin/nydusify $out/bin/nydusify --prefix PATH : ${lib.makeBinPath [ nydus ] }";
-    }; in
-runCommand "nydusify" {} ''
-  mkdir -p $out/bin
-  cp ${nydusifyRawPackage}/bin/cmd $out/bin/nydusify
-  cp ${nydusifyRawPackage}/bin/plugin $out/bin/nydusify-plugin
-''
+buildGoModule rec {
+  pname = "nydusify";
+  version = "2.3.7";
+  nativeBuildInputs = [ makeBinaryWrapper ];
+  src = fetchFromGitHub {
+    owner = "dragonflyoss";
+    repo = "nydus";
+    rev = "v${version}";
+    hash = "sha256-dVPfxsM/mduCopc2t+60+MDO98DRYvpNB+9/gUoXTSc=";
+  } + "/contrib/nydusify";
+  vendorHash = "sha256-SmDOmCMinTg72P8n4c1HnwKpt/CVeIlqdFcp5LHm2tA=";
+  postInstall = ''
+    makeWrapper $out/bin/cmd $out/bin/nydusify --prefix PATH : ${lib.makeBinPath [ nydus ] }
+    mv $out/bin/plugin $out/bin/nydusify-plugin
+  '';
+}
